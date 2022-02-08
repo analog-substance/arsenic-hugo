e45809f6dbaaf96399b92c538b06d23c8a8da9c8
defektive <defektive@users.noreply.github.com>
2022-02-08 10:56:12 -0700

    progress

---
+++ archetypes/lead.md
@@ -0,0 +1,8 @@
+{
+  "title": "",
+  "description": "",
+  "cweRefs": [],
+  "cvss": {"severity": "MEDIUM", "score": 5.0, "vector": "N/A"},
+  "exploitation": {},
+  "details": {},
+}
---
+++ static/js/arsenic.js
@@ -0,0 +1,52 @@
+
+const Arsenic = (function (){
+
+  return {
+    doLeadAction: function (action, lead) {
+      document.getElementById("errstatus").textContent = "";
+      document.getElementById("errmsg").textContent = "";
+      var http = new XMLHttpRequest();
+      var url = 'http://localhost:7433/api/lead/' + action;
+      http.open('POST', url, true);
+
+      //Send the proper header information along with the request
+      http.setRequestHeader('Content-type', 'application/json');
+
+      http.onreadystatechange = function() {
+          if(http.readyState === XMLHttpRequest.DONE) {
+              if(http.status == 200) {
+                  console.log(http.responseText);
+              } else {
+                  console.log(http.responseText);
+                  var j = JSON.parse(http.responseText);
+                  document.getElementById("errstatus").textContent = j.Status;
+                  document.getElementById("errmsg").textContent = j.Msg;
+              }
+          }
+      }
+
+      http.onerror = function() {
+          document.getElementById("errstatus").textContent = "ERROR";
+          document.getElementById("errmsg").textContent = "Unable to contact server";
+      }
+      http.send(JSON.stringify({
+        id: lead
+      }));
+    }
+  }
+})()
+
+
+jQuery(document).ready(function() {
+  $("a").attr("rel", "noopener noreferrer nofollow")
+  $("a[data-vector]").each((index, el) => { $(el).attr("href", $(el).attr("href") + $(el).data("vector") ) })
+  $(".container table:not(.table)").addClass("table")
+
+  $("button[data-lead-action]").each((index, el) => {
+    console.log(el)
+    let $el = $(el)
+    $el.click(() => {
+      Arsenic.doLeadAction($el.data("lead-action"), $el.data("lead-id"));
+    })
+  });
+})
